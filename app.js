29de56b7cfa7e411fd4206b97c8f5808e6cf2660
dmitrymitenkoff <d.mitenkoff@icloud.com>
2021-09-11 22:05:39 +1200

    Update JavaScript for form validation

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
 const form = document.querySelector('form');
 const email = document.querySelector('#email');
-const errorMessage = document.querySelector('form small');
+const errorMessage = document.querySelector('.error-msg');
 
-// Show error
 function showError() {
-  errorMessage.classList.remove('hidden');
   if (email.validity.valueMissing) {
-    // If the field is empty, display:
+    // If the field is empty, display the following error message.
     errorMessage.textContent = 'Please provide an email address';
+    email.classList.add('error');
   } else if (email.validity.typeMismatch) {
-    // If the field doesn't contain a valid email address, display:
+    // If the field doesn't contain an email address, display the following error message.
     errorMessage.textContent = 'Please provide a valid email address';
+    email.classList.add('error');
   }
-
-  // Set the styling:
-  errorMessage.classList.remove('hidden');
+  // Set the styling appropriately
+  errorMessage.classList.remove = 'hidden';
 }
 
-// EVENT LISTENERS:
-// Input field
 email.addEventListener('input', event => {
-  // Check if form field is valid
+  // Check if the form fields are valid
   if (email.validity.valid) {
-    // If there's an error message visible and if the field is valid, remove the error message:
-    errorMessage.textContent = ''; // resets the content of the message
-    errorMessage.className = 'hidden'; //resets the visual state of the message
+    // In case there is an error message visible, if the field is valid, we remove the error message.
+    errorMessage.textContent = ''; // Reset the content of the message
+    errorMessage.classList.add = 'hidden'; // Hide the error message
+    email.classList.remove('error');
   } else {
-    // If there's still an error, show the correct error
+    // If there is still an error, show the correct error
     showError();
   }
 });
 
-// Form
-form.addEventListener('submit', event => {
-  // If the email field is valid, let the form submi
+form.addEventListener('submit', function (event) {
+  // if the email field is valid, let the form submit
+
   if (!email.validity.valid) {
-    // If it isn't, display an appropriate error message
+    // If it isn't, we display an appropriate error message
     showError();
-    // Then prevent the form from being sent by cancelling the event
+    // Then we prevent the form from being sent by canceling the event
     event.preventDefault();
   }
 });
